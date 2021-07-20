92ca3469bcd928f3756a53dae5fef9ceaee00142
jojo2829 <joulinli@gmail.com>
2021-07-20 04:48:04 +0000

    question class generates questions

---
+++ questions.rb
@@ -0,0 +1,20 @@
+class Questions
+
+  def initialize(question, answer, player_answer)
+    @question = question
+    @answer = answer
+    @player_answer = player_answer
+  end
+
+  attr_accessor :question, :answer, :player_answer
+
+end
+
+def setQuestion
+  num_one = rand(1..20)
+  num_two = rand(1..20)
+  @answer = num_one + num_two
+  @question = "What does #{num_one} plus #{num_two} equal?"
+end
+
+
